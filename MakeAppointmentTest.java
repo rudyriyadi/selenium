e8e16ebd1cf3d6618df0755036b660d49f22f863
Raden Rudy Riyadi <rudy.riyadi@gmail.com>
2024-10-02 10:40:48 +0700

    Add files via upload

--- MakeAppointmentTest.java
+++ MakeAppointmentTest.java
@@ -15,28 +15,27 @@ public class MakeAppointmentTest {
     WebDriver driver;
 
 @BeforeTest
-public void init () {
-
-//initiate browser
+public void init() {
+    /* initiate browser */
     System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
     driver = new ChromeDriver();
 
 //go to homepage
-driver.navigate().to("https://katalon-demo-cura.herokuapp.com/#appointment");
+driver.navigate().to("https://katalon-demo-cura.herokuapp.com");
 driver.manage().window().maximize();
 
-//login
+/*//login
 driver.findElement(By.id("txt-username")).sendKeys("John Doe");
 driver.findElement(By.id("txt-password")).sendKeys("ThisIsNotAPassword");
 driver.findElement(By.id("btn-login")).click();
 Assert.assertEquals(driver.getCurrentUrl(),"https://katalon-demo-cura.herokuapp.com/#appointment");
-
+*/
 }
 
 @Test
-public void CheckElement() {
+public void CheckElement () {
 //check h2 element
-    Assert.assertEquals(driver.findElement(By.cssSelector("bagian h2")).getText(),"Make Appointment");
+    Assert.assertEquals(driver.findElement(By.cssSelector("section h2")).getText(),"Make Appointment");
 
 //check dropdown element
     Select dropdownFacility = new Select(driver.findElement(By.id("combo_facility")));
@@ -45,11 +44,13 @@ public void CheckElement() {
     Assert.assertEquals(dropdownoptions.get(1).getAttribute("value"),"Hongkong CURA Healthcare Center");
     Assert.assertEquals(dropdownoptions.get(2).getAttribute("values"),"Seoul CURA Healthcare Center");
 
- //check text area
+ /*//check text area
     Assert.assertEquals(driver.findElement(By.id("txt_comment")).getAttribute("placeholder"),"Comment");
-}
 
-@Test
+  */
+
+}
+@Test (priority = 0)
     public void makeAppointmentWithCorrectValues () {
 
     //check dropdown
@@ -57,9 +58,20 @@ public void CheckElement() {
     dropdownfacility.selectByValue("Seoul CURA Healthcare Center");
 
     //check box
+    driver.findElement(By.id("chk_hospotal_readmission")).click();
 
+    //check radio button
+    driver.findElement(By.id("radio_program_medicaid")).click();
 
+    //Check date picker
+    driver.findElement(By.id("txt_visit_date")).sendKeys("21/11/2024");
 
+    //check text area
+    driver.findElement (By.id("txt_comment")).sendKeys("Test text area");
+
+    //check click button
+    driver.findElement(By.id("btn-book-appointment")).click();
+    Assert.assertEquals(driver.getCurrentUrl(),"https://katalon-demo-cura.herokuapp.com/appointment.php#summary");
 
 
 }
